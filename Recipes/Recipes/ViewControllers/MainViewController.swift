4bf38bc9d143645821274dad15bfa3513acbe701
Chris Price <christopher.r.price@gmail.com>
2020-03-11 21:29:04 -0400

    UI set up.

---
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Chris Price on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func typingFinished(_ sender: UITextField) {
+    }
+    
+}
